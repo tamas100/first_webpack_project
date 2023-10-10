0f06b35e55628db7f37ae84ac53301cdb508932c
Tamás Kis <t.kis502@gmail.com>
2023-10-10 08:04:43 +0200

    create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+const path = require('path');
+module.exports = {
+    entry: './js/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
