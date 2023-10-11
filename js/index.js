d3df2a78d372951844923e1074fca8ec2760dabd
Tamás Kis <t.kis502@gmail.com>
2023-10-11 17:29:30 +0200

    add node_modules and dist to .gitignore. install bootstrap

--- js/index.js
+++ js/index.js
@@ -1,10 +1,18 @@
 import getDateString from "./date";
 import '../styles/styles.css';
 import $ from 'jquery';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
 
 $(document).ready(() => {
 
     // Print the current time
     $('.js-content').html(`It's ${getDateString()}`);
+
+    // Display the Bootstrap modal window
+    $('.js-modal').modal({
+        show: true,
+        keyboard: true
+    });;
 
 });
