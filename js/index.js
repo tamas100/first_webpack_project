f5b307c356e5933117949455f13024ce6ddde19e
Tamás Kis <t.kis502@gmail.com>
2023-10-11 13:36:44 +0200

    install jQuery

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 import getDateString from "./date";
 import '../styles/styles.css';
+import $ from 'jquery';
 
-document
-    .querySelector('.js-content')
-    .innerHTML = `It's ${getDateString()}`;
+$(document).ready(() => {
+
+    // Print the current time
+    $('.js-content').html(`It's ${getDateString()}`);
+
+});
